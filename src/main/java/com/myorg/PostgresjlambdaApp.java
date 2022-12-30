9c93065740a643cbd23365a15df87b3508a1526d
Collin Smith <collin.smith@gmail.com>
2022-12-30 10:15:20 -0700

    Initial checkin for Postgres Java Lambda

--- src/main/java/com/myorg/PostgresjlambdaApp.java
+++ src/main/java/com/myorg/PostgresjlambdaApp.java
@@ -10,7 +10,7 @@ public class PostgresjlambdaApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new PostgresjlambdaStack(app, "PostgresjlambdaStack", StackProps.builder()
+        new PostgresjlambdaStack(app, "PostgresjlambdaStack", StackProps.builder()  
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
